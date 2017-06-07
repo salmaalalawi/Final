d728dde0f53c0f387a528e3598e470c275f4dd92
salmaalalawi <soo2011so@gmail.com>
2017-06-07 10:33:58 +0400

    Initial commit

---
+++ app/src/main/java/com/example/fatma/fainalproject1/man_trainer.java
@@ -0,0 +1,13 @@
+package com.example.fatma.fainalproject1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class man_trainer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_man_trainer);
+    }
+}
---
+++ app/src/main/java/com/example/fatma/fainalproject1/trainig_session.java
@@ -0,0 +1,13 @@
+package com.example.fatma.fainalproject1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class trainig_session extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trainig_session);
+    }
+}
